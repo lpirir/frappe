0e5dfbf9507406ade5e4ba3df05c084ed16edbf3
Nabin Hait <nabinhait@gmail.com>
2017-11-17 12:43:33 +0530

    Revert "[fix] do not translate domain records (#4507)"
    This reverts commit a6f0f65763c02457c204f38dae64d139c4deb4a9.

--- frappe/core/doctype/domain/domain.py
+++ frappe/core/doctype/domain/domain.py
@@ -34,13 +34,11 @@ class Domain(Document):
 	def remove_domain(self):
 		'''Unset domain settings'''
 		self.setup_data()
-
-		if self.data.restricted_roles:
-			for role_name in self.data.restricted_roles:
-				if frappe.db.exists('Role', role_name):
-					role = frappe.get_doc('Role', role_name)
-					role.disabled = 1
-					role.save()
+		for role_name in self.data.restricted_roles:
+			if frappe.db.exists('Role', role_name):
+				role = frappe.get_doc('Role', role_name)
+				role.disabled = 1
+				role.save()
 
 		if self.data.custom_fields:
 			for doctype in self.data.custom_fields:
